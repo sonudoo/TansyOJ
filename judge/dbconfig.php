eca2fb6e1e9093b662ccec6bd460e6d8080cec8e
Sushant Kumar Gupta <sonudoo@users.noreply.github.com>
2017-09-08 19:43:36 +0530

    Update dbconfig.php

--- judge/dbconfig.php
+++ judge/dbconfig.php
@@ -1,5 +1,7 @@
 <?php
-$conn = mysqli_connect("your_mysql_server_here","your_database_username_here","your_mysql_password_here","tansy_main");
+
+// Replace your database servername, username, password, database name here
+$conn = mysqli_connect("your_mysql_server_here","your_database_username_here","your_mysql_password_here","your_database_name_here");
 if(!$conn){
 	die("An unknown error occured while connecting to the database");
 }
